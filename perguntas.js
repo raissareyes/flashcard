cf8e4b164802719d294189b689af8bf8f11d6a79
raissareyes <00001082176965sp@al.educacao.sp.gov.br>
2024-11-25 10:00:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 16?',
+    '4'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a velocidade da luz no vácuo?',
+    '299.792.458 m/s'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Qual a importancia da inovação em seu négocio ?',
+    ' A inovação ajuda a diferenciar a empresa, aumentar competitividade e atrair clientes'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a formula química do metano?',
+    'CH4'
+)
